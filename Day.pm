6e7c3197e0e0552ea3f283f00e602a09c700bfc7
Michal Špaček <tupinek@gmail.com>
2013-09-20 21:24:28 +0200

    Prepare for 0.03 version.

--- Day.pm
+++ Day.pm
@@ -11,7 +11,7 @@ use DateTime::Event::Random;
 use DateTime::Event::Recurrence;
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -339,6 +339,6 @@ BSD license.
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
